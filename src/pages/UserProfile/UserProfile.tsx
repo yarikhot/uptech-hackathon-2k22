4285ec6187b57d9d16f4380ae3fc1ad06e47c55e
Maksym Kalashnyk <maxim.kalashnyk@gmail.com>
2022-09-18 09:37:45 +0300

    fix: updated modal displaying

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Avatar, Box, Button, Typography, IconButton } from '@mui/material';
 import StarPurple500OutlinedIcon from '@mui/icons-material/StarPurple500Outlined';
 import CurrencyYenOutlinedIcon from '@mui/icons-material/CurrencyYenOutlined';
@@ -11,6 +11,7 @@ import { useProfile } from '@hooks/useProfile';
 
 import { palette } from '@styles/palette';
 import { DealTypes } from '@types';
+import { AddDealModal } from './AddDealModal';
 
 const DealRow = styled.div`
   height: 70px;
@@ -42,62 +43,78 @@ const mock = [
 export const UserProfile: FC = () => {
   const { profile } = useProfile();
 
+  const [isModalOpen, setOpenModal] = useState(false);
+  const onOpenModal = () => setOpenModal(true);
+  const onCloseModal = () => setOpenModal(false);
+
   return (
-    <Box display="flex" flexDirection="column" mt="16px">
-      <Box mt="32px">
-        <Typography variant="h4" mb="12px">
-          Profile
-        </Typography>
-        <Box
-          sx={{
-            padding: '12px 16px',
-            background: palette.backgrounds.primary,
-            borderRadius: '12px',
-            position: 'relative',
-          }}
-        >
-          <Box display="flex" alignItems="center">
-            <Avatar
-              sizes="70px"
-              sx={{ height: '80px', width: '80px', mr: '16px' }}
-              src={profile.imageUrl}
-            />
-            <Box>
-              <Typography variant="h7">{profile.userName}</Typography>
-              <br />
-              <Typography>{profile.email}</Typography>
-              <Box display="flex" alignItems="center">
-                <StarPurple500OutlinedIcon />
-                {profile.score}
-                <CurrencyYenOutlinedIcon />
-                {profile.coinAmount}
+    <>
+      <Box display="flex" flexDirection="column" mt="16px">
+        <Box mt="32px">
+          <Typography variant="h4" mb="12px">
+            Profile
+          </Typography>
+          <Box
+            sx={{
+              padding: '12px 16px',
+              background: palette.backgrounds.primary,
+              borderRadius: '12px',
+              position: 'relative',
+            }}
+          >
+            <Box display="flex" alignItems="center">
+              <Avatar
+                sizes="70px"
+                sx={{ height: '80px', width: '80px', mr: '16px' }}
+                src={profile.imageUrl}
+              />
+              <Box>
+                <Typography variant="h7">{profile.userName}</Typography>
+                <br />
+                <Typography>{profile.email}</Typography>
+                <Box display="flex" alignItems="center">
+                  <StarPurple500OutlinedIcon />
+                  {profile.score}
+                  <CurrencyYenOutlinedIcon />
+                  {profile.coinAmount}
+                </Box>
               </Box>
             </Box>
+            <IconButton sx={{ position: 'absolute', top: '12px', right: '16px' }}>
+              <SettingsSuggestOutlinedIcon />
+            </IconButton>
           </Box>
-          <IconButton sx={{ position: 'absolute', top: '12px', right: '16px' }}>
-            <SettingsSuggestOutlinedIcon />
-          </IconButton>
-        </Box>
-        <Box mb="32px" display="flex" mt="24px" alignItems="center" justifyContent="space-between">
-          <Typography variant="h4">List of your deals</Typography>
-          <Button variant="contained" endIcon={<AddCircleOutlinedIcon />}>
-            Add you deal
-          </Button>
-        </Box>
-        {mock.map(({ id, deal, date, amount }) => (
-          <DealRow key={id}>
-            <Typography className="item">{id}</Typography>
-            <Typography className="item">{deal}</Typography>
-            <Typography className="item">{date}</Typography>
-            <Typography className="item" display="flex" alignItems="center">
-              {amount}{' '}
-              <Typography variant="body1" sx={{ fontSize: '28px', lineHeight: '28px', ml: '12px' }}>
-                ⭐️
+          <Box
+            mb="32px"
+            display="flex"
+            mt="24px"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Typography variant="h4">List of your deals</Typography>
+            <Button variant="contained" endIcon={<AddCircleOutlinedIcon />} onClick={onOpenModal}>
+              Add you deal
+            </Button>
+          </Box>
+          {mock.map(({ id, deal, date, amount }) => (
+            <DealRow key={id}>
+              <Typography className="item">{id}</Typography>
+              <Typography className="item">{deal}</Typography>
+              <Typography className="item">{date}</Typography>
+              <Typography className="item" display="flex" alignItems="center">
+                {amount}{' '}
+                <Typography
+                  variant="body1"
+                  sx={{ fontSize: '28px', lineHeight: '28px', ml: '12px' }}
+                >
+                  ⭐️
+                </Typography>
               </Typography>
-            </Typography>
-          </DealRow>
-        ))}
+            </DealRow>
+          ))}
+        </Box>
       </Box>
-    </Box>
+      <AddDealModal isOpen={isModalOpen} onClose={onCloseModal} />
+    </>
   );
 };
